8d9261a1249f8a4e95460192082a5228751084c9
vallabh2920 <vallabhingle2920@gmail.com>
2021-07-23 23:21:08 +0530

    80% done.

--- components/contact/Contact.js
+++ components/contact/Contact.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Contact = () => {
     return (
-        <div className="flex justify-evenly mb-24 ">
+        <div>
+            <h2 className="text-fourth font-extrabold text-4xl tracking-wider ">Contact me</h2>
+            <div className="flex justify-evenly mb-24 ">
            <div className="">
                <img src="/img/contact.svg" className="px-11  w-photo">
                </img>
@@ -12,6 +14,7 @@ const Contact = () => {
                <input type="text" placeholder="Email" className="mt-6"></input>
                <textarea placeholder="Message" className=" mt-6 h-32"></textarea>
                <input type="submit" className="mt-6 rounded-md h-10 border-2 border-fourth bg-third text-white"></input>
+           </div>
            </div>
         </div>
     )
