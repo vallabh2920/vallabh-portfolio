b66c29e115117e86ad5d58c0357239ecb8248298
vallabh2920 <vallabhingle2920@gmail.com>
2022-01-21 21:10:18 +0530

    new resume

--- pages/my-sitemap.js
+++ pages/my-sitemap.js
@@ -1,31 +1,31 @@
-import { NextApiRequest, NextApiResponse } from "next";
+// import { NextApiRequest, NextApiResponse } from "next";
 
-const { SitemapStream, streamToPromise } = require("sitemap");
-const { Readable } = require("stream");
+// const { SitemapStream, streamToPromise } = require("sitemap");
+// const { Readable } = require("stream");
 
-export default async (req, res) => {
-  // An array with your links
-  const links = [
-    { url: "/", changefreq: "daily", priority: 0.3 },
-    { url: "/#about", changefreq: "daily", priority: 0.3 },
-    { url: "/#skills", changefreq: "daily", priority: 0.3 },
-    { url: "/#projects", changefreq: "daily", priority: 0.3 },
-    { url: "/#contact", changefreq: "daily", priority: 0.3 },
-  ];
+// export default async (req, res) => {
+//   // An array with your links
+//   const links = [
+//     { url: "/", changefreq: "daily", priority: 0.3 },
+//     { url: "/#about", changefreq: "daily", priority: 0.3 },
+//     { url: "/#skills", changefreq: "daily", priority: 0.3 },
+//     { url: "/#projects", changefreq: "daily", priority: 0.3 },
+//     { url: "/#contact", changefreq: "daily", priority: 0.3 },
+//   ];
 
-  // Create a stream to write to
-  const stream = new SitemapStream({
-    hostname: `https://${req.headers.host}`,
-    cacheTime: 600000,
-  });
+//   // Create a stream to write to
+//   const stream = new SitemapStream({
+//     hostname: `https://${req.headers.host}`,
+//     cacheTime: 600000,
+//   });
 
-  res.writeHead(200, {
-    "Content-Type": "application/xml",
-  });
+//   res.writeHead(200, {
+//     "Content-Type": "application/xml",
+//   });
 
-  const xmlString = await streamToPromise(
-    Readable.from(links).pipe(stream)
-  ).then((data) => data.toString());
+//   const xmlString = await streamToPromise(
+//     Readable.from(links).pipe(stream)
+//   ).then((data) => data.toString());
 
-  res.end(xmlString);
-};
+//   res.end(xmlString);
+// };
